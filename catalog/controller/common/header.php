766723297ef1bc09864f34feec8064472c3649a4
Klim <catcherochek@yandex.ru>
2019-07-12 18:43:47 +0300

    cammulative_patch

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -129,7 +129,8 @@ class ControllerCommonHeader extends Controller {
 		$data['currency'] = $this->load->controller('common/currency');
 		$data['search'] = $this->load->controller('common/search');
 		$data['cart'] = $this->load->controller('common/cart');
-
+		$this->load->controller('extension/module/ocmegamenu');
+        $data['menu'] = 'menu here';
 		// For page specific css
 		if (isset($this->request->get['route'])) {
 			if (isset($this->request->get['product_id'])) {
